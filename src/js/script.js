3ef7bd6695ec06b8333fa9c5576c085df1b0ca08
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-07 09:51:39 -0300

    Atualizando o projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,29 @@
+window.onload = function(){
+ 
+    const intro=document.getElementById("inicio");
+ 
+    setTimeout(()=>{
+        intro.classList.add("efeito-out")
+        setTimeout(()=>{
+            intro.style.display="none";
+            content.classList.remove("hidden");
+            content.classList.add("efeito-in");
+        },1000)
+    },3000)
+}
+ 
 function trocar(cor){
     document.body.style.background = cor;
 }
+//MENU HAMBURGUER
+    document.addEventListener('DOMContentLoaded',()=>{
+        const menuIcone= document.getWlementById("menu-icone");
+        const navMenu= document.querySelector("nav");
+        if(menuIcone && navMenu){
+            menuIcone.onclick=()=>{
+                navMenu.classList.toogle('active')
+                menuIcone.classList.toogle('open')
+            }
+        }
+ 
+})
